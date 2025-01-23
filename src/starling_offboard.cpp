3236ad9ff8f6eb01508d6a43764477bf68f36740
fred <fred.vatnsdal@gmail.com>
2025-01-23 11:35:52 -0500

    Fill body of param getters but leave calls commented

--- src/starling_offboard.cpp
+++ src/starling_offboard.cpp
@@ -53,19 +53,85 @@ void StarlingOffboard::GetNodeParameters() {
 }
 
 void StarlingOffboard::GetMissionControl(){
-    while (!mission_control_received_) {
-        sleep(1);
-        RCLCPP_WARN_ONCE(this->get_logger(), "Waiting for mission control topic...");
+  sync_parameters_client_ = std::make_shared<rclcpp::SyncParametersClient>(this, "/mission_control");
+  while (!sync_parameters_client_->wait_for_service(1s)) {
+    if (!rclcpp::ok()) {
+      RCLCPP_ERROR(this->get_logger(), "Interrupted while waiting for the service. Exiting.");
+      rclcpp::shutdown();
     }
-    RCLCPP_INFO(this->get_logger(), "Mission control received.");
+    RCLCPP_INFO(this->get_logger(), "service not available, waiting again...");
+  }
+  
+  auto result = sync_parameters_client_->get_parameters({"enable", "takeoff", "land"});
+  RCLCPP_INFO(this->get_logger(), "result size = %ld", result.size());
+  for (auto &p : result) {
+    if (p.get_name() == "enable") {
+      enable_ = p.get_value<bool>();
+      RCLCPP_INFO(this->get_logger(), "init enable = %d", enable_);
+    }
+    if (p.get_name() == "takeoff") {
+      takeoff_ = p.get_value<bool>();
+      RCLCPP_INFO(this->get_logger(), "init takeoff = %d", takeoff_);
+    }
+    if (p.get_name() == "land") {
+      land_ = p.get_value<bool>();
+      RCLCPP_INFO(this->get_logger(), "init land = %d", land_);
+    }
+  }
+  
+  mission_control_PEH_ptr_ =
+      std::make_shared<rclcpp::ParameterEventHandler>(this);
+  
+  auto cb_enable = [this](const rclcpp::Parameter& p) {
+    enable_ = p.get_value<bool>();
+    RCLCPP_INFO(this->get_logger(), "received enable = %d", enable_);
+  };
+  auto cb_takeoff = [this](const rclcpp::Parameter& p) {
+    takeoff_ = p.get_value<bool>();
+    RCLCPP_INFO(this->get_logger(), "received takeoff = %d", takeoff_);
+  };
+  auto cb_land = [this](const rclcpp::Parameter& p) {
+    land_ = p.get_value<bool>();
+    RCLCPP_INFO(this->get_logger(), "received land = %d", land_);
+  };
+  handle_enable_= mission_control_PEH_ptr_->add_parameter_callback(
+      "enable", cb_enable, "/mission_control");
+  handle_takeoff_= mission_control_PEH_ptr_->add_parameter_callback(
+      "takeoff", cb_takeoff, "/mission_control");
+  handle_land_= mission_control_PEH_ptr_->add_parameter_callback(
+      "land", cb_land, "/mission_control");
 }
 
 void StarlingOffboard::GetMissionOriginGPS() {
-  while (!origin_gps_received_) {
-      sleep(1);
-      RCLCPP_WARN_ONCE(this->get_logger(), "Waiting for mission origin GPS...");
+  std::string service_name = "/pac_gcs/mission_origin_gps/get_parameters";
+  auto pac_gcs_parameters_client =
+      this->create_client<rcl_interfaces::srv::GetParameters>(service_name);
+  while (!pac_gcs_parameters_client->wait_for_service(1s)) {
+    if (!rclcpp::ok()) {
+      rclcpp::shutdown();
+    }
   }
-  RCLCPP_INFO(this->get_logger(), "Mission origin GPS received.");
+  auto request =
+      std::make_shared<rcl_interfaces::srv::GetParameters::Request>();
+  request->names = {"mission_origin_lat", "mission_origin_lon", "heading"};
+  while (!origin_gps_received_) {
+    auto result_future = pac_gcs_parameters_client->async_send_request(request);
+    if (rclcpp::spin_until_future_complete(this->get_node_base_interface(),
+                                           result_future) !=
+        rclcpp::FutureReturnCode::SUCCESS) {
+      rclcpp::sleep_for(1s);
+      continue;
+    }
+    auto result = result_future.get();
+    if (result->values.size() == 0) {
+      rclcpp::sleep_for(1s);
+      continue;
+    }
+    mission_origin_lat_ = result->values[0].double_value;
+    mission_origin_lon_ = result->values[1].double_value;
+    heading_ = result->values[2].double_value;
+    origin_gps_received_ = true;
+  }
 }
 
 void StarlingOffboard::GetLaunchGPS() {
